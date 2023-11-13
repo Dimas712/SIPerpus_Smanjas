25e06d495e3c4b5ee7bae855acf966ecedb1276f
Dimas712 <jasinga1933@gmail.com>
2023-11-13 22:49:11 +0700

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import './App.css'
+import Login from './Components/Login'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Login/>
   )
 }
 
