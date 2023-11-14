95cb2dd7c1adbaef233481a9cc87213152edb2aa
Dimas712 <jasinga1933@gmail.com>
2023-11-14 12:23:22 +0700

    edit

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,13 +1,15 @@
-
+import React from 'react'
 
 function Login()  {
-    return (
-        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  return (
+    <div>
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
           <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
           <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
         </div>
       
+
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form class="space-y-6" action="#" method="POST">
             <div>
@@ -40,7 +42,8 @@ function Login()  {
           </p>
         </div>
       </div>
-    )
-  }
-  
+      </div>
+  );
+};
+
 export default Login
