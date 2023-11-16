06fe88c2e13f5b012ab175e460141f8c93eceb71
unknown <rezaafriansyah@gmail.com>
2023-11-16 17:48:27 +0700

    menambahkan sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import Login from './Components/Login'
 import Navbar from './Components/Navbar'
+import Sidebar from './Components/Sidebar'
+
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Login/>
-    </>
+    <Sidebar/>
   )
 }
 
