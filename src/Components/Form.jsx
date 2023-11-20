44a4b365acf6615510c583280b22e138a5250fae
RezaAFR11 <rezaafriansyah@gmail.com>
2023-11-20 23:46:21 +0700

    edit dikit

---
+++ src/Components/Form.jsx
@@ -0,0 +1,108 @@
+import React, {useState} from "react"
+import Sidebar from "./Sidebar";
+import { FaPlus } from "react-icons/fa6";
+
+
+const Form = () => {
+    const [formData, setFormData] = useState({
+        noPeminjam: '',
+        nama: '',
+        kelas: '',
+        tglPinjam: '',
+        tglKembali: '',
+        status: ''
+    });
+
+    // Handle untuk mengubah state saat nilai input berubah
+    const handleChange = (e) =>{
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    // Handle untuk mengirim formulir 
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        //Melakukan sesuatu dengan data form, untuk mengirim data ke server atau validasi
+        console.log('Data Formulir', formData);
+    };
+
+    return(
+        <div className="flex flex-row">
+            <Sidebar />
+            <div className="px-5 py-10 w-screen">
+                <a>
+                    <FaPlus />
+                    <p></p>
+                </a>
+                <form className="flex flex-col bg-putih rounded-3xl py-10">
+                    <div className="form-coun">
+                        <label className="form-label">No Peminjam</label>
+                        <input
+                        type="number"
+                        name="noPeminjam"
+                        value={formData.noPeminjam}
+                        onChange={handleChange}
+                        className="form-input"/>
+                    </div>
+                    <div className="form-coun">
+                        <label className="form-label">Nama</label>
+                        <input
+                        type="text"
+                        name="nama"
+                        value={formData.nama}
+                        onChange={handleChange}
+                        className="form-input"/>
+                    </div>
+                    <divn className="form-coun">
+                        <label className="form-label">Kelas</label>
+                        <input
+                        type="text"
+                        name="kelas"
+                        value={formData.kelas}
+                        onChange={handleChange}
+                        className="form-input"/>
+                    </divn>
+                    <div className="form-coun">
+                        <label className="form-label">Tanggal Pinjam</label>
+                        <input
+                        type="date"
+                        name="tglPinjam"
+                        value={formData.tglPinjam}
+                        onChange={handleChange}
+                        className="form-input"/>
+                    </div>
+                    <div className="form-coun">
+                        <label className="form-label">Tanggal Kembali</label>
+                        <input
+                        type="date"
+                        name="tglKembali"
+                        value={formData.tglKembali}
+                        onChange={handleChange}
+                        className="form-input"/>
+                    </div>
+                    <div className="form-coun">
+                        <label className="form-label">Status</label>
+                        <input
+                        type="text"
+                        name="status"
+                        value={formData.status}
+                        onChange={handleChange}
+                        className="form-input"/>
+                    </div>
+                    <div className="flex justify-end mr-20 ">
+                        <button
+                        type="submit"
+                        onClick={handleSubmit}
+                        className="bg-green3 border-solid rounded-full text-base px-7 py-1 font-bold text-white hover:bg-green">
+                            Pinjam
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Form;
--- src/Components/ProfilSiswa.jsx
+++ src/Components/ProfilSiswa.jsx
@@ -1,43 +1,82 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
-import {ListBulletIcon} from '@heroicons/react/24/outline'
+import {UserCircleIcon} from '@heroicons/react/24/outline'
 import Foto3x4 from '../assets/foto3x4.jpg'
+import Footer from './Footer';
  
 const ProfilSiswa = () => {
   return (
     <div>
       <Navbar />
-      <div className='flex h-screen border-2 border-red-500'>
+      <div className='flex h-screen'>
         <Sidebar />
-        <div className='static border-2 w-full h-screen border-black p-10 pl-32 pr-20'>
-          <div className='relative border '>
-            <div className='bg-green2 h-16 rounded-3xl flex items-center space-x-16 pl-10'>
-              <ListBulletIcon className='h-10 w-10' strokeWidth={2} color='white'/>
-              <p className='text-slate-50 font-noto font-semibold text-2xl'>Pembaruan Profil</p>
+        <div className='border-2 w-full h-screen p-10 pl-5 md:pl-32 pr-5 md:pr-20'>
+          <div className='relative'>
+            <div className='bg-green2 h-16 rounded-3xl flex items-center space-x-10 md:pl-6'>
+              <UserCircleIcon className='h-10 w-10' color='white'/>
+              <p className='text-slate-50 font-noto font-semibold text-3xl'>Pembaruan Profil</p>
             </div>
-            <div className='mt-20 flex flex-row'>
+            <div className='mt-20 flex flex-col md:flex-row'>
                 <img
-                className='h-52 w-44'
+                className='h-54 w-48 mb-4 md:mb-0 md:mr-0'
                 src={Foto3x4} 
                 alt="foto siswa" 
                 />
-                <div className='pl-20 font-semibold'>
+                <div className='md:pl-20 font-semibold'>
                     <ul>
-                        <li className='mb-1.5'>Nama : Mumtazah</li>
-                        <li className='mb-1.5'>Password : infinitelearning</li>
-                        <li className='mb-1.5'>Tanggal Lahir : 27 Juni 1945</li>
-                        <li className='mb-1.5'>Tempat Lahir : Bogor </li>
-                        <li className='mb-1.5'>No Telepon : 08321314171</li>
-                        <li className='mb-1.5'>Email : mumum221@gmail.com</li>
-                        <li className='mb-1.5'>Alamat : Bogor</li>   
+                        <li className='mb-1.5'>
+                            <span style={{ minWidth: '120px', display: 'inline-block' }}>Nama</span>
+                            <span>:</span>
+                            <span className='ml-4'>Mumtazah</span>
+                        </li>
+                        <li className='mb-1.5'>
+                            <span style={{ minWidth: '120px', display: 'inline-block' }}>NIS</span>
+                            <span>:</span>
+                            <span className='ml-4'>00001</span>
+                        </li>
+                        <li className='mb-1.5'>
+                            <span style={{ minWidth: '120px', display: 'inline-block' }}>Password</span>
+                            <span>:</span>
+                            <span className='ml-4'>infinitelearning</span>
+                        </li>
+                        <li className='mb-1.5'>
+                            <span style={{ minWidth: '120px', display: 'inline-block' }}>Tanggal Lahir</span>
+                            <span>:</span>
+                            <span className='ml-4'>27 Juni 1945</span>
+                        </li>
+                        <li className='mb-1.5'>
+                            <span style={{ minWidth: '120px', display: 'inline-block' }}>Tempat Lahir</span>
+                            <span>:</span>
+                            <span className='ml-4'>Bogor</span>
+                        </li>
+                        <li className='mb-1.5'>
+                            <span style={{ minWidth: '120px', display: 'inline-block' }}>No Telepon</span>
+                            <span>:</span>
+                            <span className='ml-4'>08321314171</span>
+                        </li>
+                        <li className='mb-1.5'>
+                            <span style={{ minWidth: '120px', display: 'inline-block' }}>Email</span>
+                            <span>:</span>
+                            <span className='ml-4'>mumum221@gmail.com</span>
+                        </li>
+                        <li className='mb-1.5'>
+                            <span style={{ minWidth: '120px', display: 'inline-block' }}>Alamat</span>
+                            <span>:</span>
+                            <span className='ml-4'>Bogor</span>
+                        </li>   
                     </ul>
                 </div>
             </div>
-        
+
+            <div className='absolute bottom-0 right-0 mb-4 mr-4'>
+              <button className='bg-blue-500 hover:bg-blue-800 text-white px-4 py-2 rounded-md'>Edit</button>
+            </div>
+
           </div>
         </div>
       </div>
+      <Footer/>
     </div>
   );
 };
