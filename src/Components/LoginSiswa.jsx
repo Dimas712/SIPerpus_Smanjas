626b45cc614265456ff0e3e75efb55852fa4c991
Dimas712 <jasinga1933@gmail.com>
2023-12-03 10:31:43 +0700

    edit

--- src/Components/LoginSiswa.jsx
+++ src/Components/LoginSiswa.jsx
@@ -92,7 +92,7 @@ function LoginSiswa()  {
               <button
               onClick={handleLogin}
                 type="button"
-                className="flex w-full justify-center rounded-md bg-green4 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-darkgreen-600"
+                className="flex w-full justify-center rounded-md bg-green4 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-darkgreen-600"
               >
                 Masuk
               </button>
