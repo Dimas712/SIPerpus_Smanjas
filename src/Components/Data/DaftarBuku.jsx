5cd6ba4f0c66cc70811eb1b7f43eba2be3f95ef6
Dimas712 <jasinga1933@gmail.com>
2023-12-05 10:33:33 +0700

    menambahkan daftarbuku

---
+++ src/Components/Data/DaftarBuku.jsx
@@ -0,0 +1,48 @@
+
+const DaftarBuku = [
+    {
+        id: 1,
+        title: "Dilan 1990",
+        sinopsis: "Novel berjudul Dia Adalah Dilanku Tahun 1990 merupakan karya dari Pidi Baiq. Dilan Dia Adalan Dilanku menceritakan percintaan anak SMA yang cukup unik. Novel dengan tebal 348 halaman ini diterbitkan pada tahun 2014. Ada beberapa penerbit yang telah menerbitkan novel Dilan 1990, yakni penerbit Pastel Books pada 2014 dan penerbit Mizan Pustaka pada 2015. Baca selengkapnya di artikel Sinopsis Novel 'Dilan' Dia Adalah Dilanku Tahun 1990 - Pidi Baiq.",
+        stock: 10,
+        ISSBN: 20056,
+        penulis: "Pidi Baiq",
+        penerbit:"Pastel Books (Mizan Group)",
+        tahun: 2015, 
+    },
+
+    {
+        id: 2,
+        title: "Seni Budaya",
+        sinopsis: "Novel berjudul Dia Adalah Dilanku Tahun 1990 merupakan karya dari Pidi Baiq. Dilan Dia Adalan Dilanku menceritakan percintaan anak SMA yang cukup unik. Novel dengan tebal 348 halaman ini diterbitkan pada tahun 2014. Ada beberapa penerbit yang telah menerbitkan novel Dilan 1990, yakni penerbit Pastel Books pada 2014 dan penerbit Mizan Pustaka pada 2015. Baca selengkapnya di artikel Sinopsis Novel 'Dilan' Dia Adalah Dilanku Tahun 1990 - Pidi Baiq.",
+        stock: 10,
+        ISSBN: 20056,
+        penulis: "Pidi Baiq",
+        penerbit:"Airlangga",
+        tahun: 2015, 
+    },
+
+    {
+        id: 3,
+        title: "Matematika",
+        sinopsis: "Novel berjudul Dia Adalah Dilanku Tahun 1990 merupakan karya dari Pidi Baiq. Dilan Dia Adalan Dilanku menceritakan percintaan anak SMA yang cukup unik. Novel dengan tebal 348 halaman ini diterbitkan pada tahun 2014. Ada beberapa penerbit yang telah menerbitkan novel Dilan 1990, yakni penerbit Pastel Books pada 2014 dan penerbit Mizan Pustaka pada 2015. Baca selengkapnya di artikel Sinopsis Novel 'Dilan' Dia Adalah Dilanku Tahun 1990 - Pidi Baiq.",
+        stock: 10,
+        ISSBN: 20056,
+        penulis: "Pidi Baiq",
+        penerbit:"Airlangga",
+        tahun: 2015, 
+    },
+
+    {
+        id: 4,
+        title: "Kimia",
+        sinopsis: "Novel berjudul Dia Adalah Dilanku Tahun 1990 merupakan karya dari Pidi Baiq. Dilan Dia Adalan Dilanku menceritakan percintaan anak SMA yang cukup unik. Novel dengan tebal 348 halaman ini diterbitkan pada tahun 2014. Ada beberapa penerbit yang telah menerbitkan novel Dilan 1990, yakni penerbit Pastel Books pada 2014 dan penerbit Mizan Pustaka pada 2015. Baca selengkapnya di artikel Sinopsis Novel 'Dilan' Dia Adalah Dilanku Tahun 1990 - Pidi Baiq.",
+        stock: 10,
+        ISSBN: 20056,
+        penulis: "Pidi Baiq",
+        penerbit:"Airlangga",
+        tahun: 2015, 
+    },
+]
+
+export default DaftarBuku;
